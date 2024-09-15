b71f296453a3ebb8d983f8485be5ea818ccf15c0
HentaiMaster <gabriel.03@live.se>
2024-09-16 01:57:51 +0200

    Have almost gotten everything to work, It just says that when I run runData that it is looking for the loottable of allay, but cannot find it...

--- src/main/java/net/dao/mini_kingdoms/datagen/MKDataGenerator.java
+++ src/main/java/net/dao/mini_kingdoms/datagen/MKDataGenerator.java
@@ -4,6 +4,7 @@ import net.dao.mini_kingdoms.Mini_kingdoms;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.EntityLootSubProvider;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.data.tags.EntityTypeTagsProvider;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSet;
@@ -28,15 +29,18 @@ public class MKDataGenerator {
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
 
         generator.addProvider(event.includeServer(), new LootTableProvider(packOutput, Collections.emptySet(),
-                List.of(new LootTableProvider.SubProviderEntry(MKBlockLootTableProvider::new, LootContextParamSets.BLOCK)), lookupProvider));
+                List.of(new LootTableProvider.SubProviderEntry(MKBlockLootTableProvider::new, LootContextParamSets.BLOCK), new LootTableProvider.SubProviderEntry(MKEntityLootTableProvider::new, LootContextParamSets.ENTITY)), lookupProvider));
+        //generator.addProvider(event.includeServer(), new LootTableProvider(packOutput, Collections.emptySet(),      List.of(new LootTableProvider.SubProviderEntry(MKEntityLootTableProvider::new,
+                //LootContextParamSets.ENTITY)), lookupProvider));
 
         generator.addProvider(event.includeServer(), new MKRecipeProvider(packOutput, lookupProvider));
-        generator.addProvider(event.includeServer(), new MKEntityLootTableProvider(packOutput, Collections.emptySet(),List.of(new LootTableProvider.SubProviderEntry(MKEntityLootTableProvider::new, LootContextParamSets.ENTITY)), lookupProvider));
 
         BlockTagsProvider blockTagsProvider = new MKBlockTagProvider(packOutput, lookupProvider, existingFileHelper);
         generator.addProvider(event.includeServer(), blockTagsProvider);
         generator.addProvider(event.includeServer(), new MKItemTagProvider(packOutput, lookupProvider, blockTagsProvider.contentsGetter(), existingFileHelper));
         EntityTypeTagsProvider entityTypeTagsProvider = new MKEntityTagProvider(packOutput, lookupProvider);
+        /*generator.addProvider(event.includeServer(), new EntityTypeTagsProvider(packOutput, lookupProvider,
+                entityTypeTagsProvider.contentsGetter(), existingFileHelper));*/
 
 
         generator.addProvider(event.includeClient(), new MKBlockStateProvider(packOutput, existingFileHelper));
--- src/main/java/net/dao/mini_kingdoms/datagen/MKEntityLootTableProvider.java
+++ src/main/java/net/dao/mini_kingdoms/datagen/MKEntityLootTableProvider.java
@@ -2,6 +2,7 @@ package net.dao.mini_kingdoms.datagen;
 
 import net.dao.mini_kingdoms.items.MKItems;
 import net.dao.mini_kingdoms.util.MKTags;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.EntityLootSubProvider;
 import net.minecraft.data.tags.EntityTypeTagsProvider;
@@ -9,6 +10,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.EntityTypeTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
@@ -17,25 +19,29 @@ import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class MKEntityLootTableProvider extends EntityLootSubProvider {
 
-    public MKEntityLootTableProvider(PackOutput output){
-        super(output, Set.of(), LootContextParamSets.ENTITY);
+    protected MKEntityLootTableProvider(HolderLookup.Provider pRegistries) {
+        super(FeatureFlags.REGISTRY.allFlags(), pRegistries);
     }
     @Override
     public void generate() {
+        // Combine custom and vanilla loot tables
+        addLootTableForEntity(EntityType.ARMADILLO, createArmadilloLootTable());
+        addLootTableForEntity(EntityType.TURTLE, createTurtleLootTable());
+        addLootTableForEntity(EntityType.BLAZE, createBlazeLootTable());
+        addLootTableForEntity(EntityType.DOLPHIN, createDolphinLootTable());
+        addLootTableForEntity(EntityType.RABBIT, createRabbitLootTable());
+        addLootTableForEntity(EntityType.HORSE, createHorseLootTable());
+        addLootTableForEntity(EntityType.GLOW_SQUID, createGlowSquidLootTable());
 
-        add(EntityType.ARMADILLO, createArmadilloLootTable());
-        add(EntityType.TURTLE, createTurtleLootTable());
-        add(EntityType.BLAZE, createBlazeLootTable());
-        add(EntityType.DOLPHIN, createDolphinLootTable());
-        add(EntityType.RABBIT, createRabbitLootTable());
-        add(EntityType.HORSE, createHorseLootTable());
-        add(EntityType.GLOW_SQUID, createGlowSquidLootTable());
 
-        // Add loot tables for entities in custom tags
+        // Apply custom loot tables for entities in your custom tags
         applyLootTableForTag(MKTags.MKEntity.FEET, createFeetLootTable());
         applyLootTableForTag(MKTags.MKEntity.LOOTZOMBIE, createLootZombieLootTable());
         applyLootTableForTag(MKTags.MKEntity.BEARMEAT, createBearLootTable());
@@ -43,7 +49,97 @@ public class MKEntityLootTableProvider extends EntityLootSubProvider {
         applyLootTableForTag(MKTags.MKEntity.ORGANMOBS, createOrganLootTable());
     }
 
-    private LootTable.Builder createOrganLootTable() {
+    // Helper function to create a loot table for a specific entity
+    protected void addLootTableForEntity(EntityType<?> entityType, LootTable.Builder customLoot) {
+        // Create a new LootTable with a new pool
+        LootTable.Builder lootTableBuilder = LootTable.lootTable()
+                .withPool(LootPool.lootPool()
+                        .setRolls(ConstantValue.exactly(1))
+                        .add(LootItem.lootTableItem(MKItems.GLOW_SQUID_EYE.get()).setWeight(10))
+                        .add(LootItem.lootTableItem(MKItems.BEAR_MEAT.get()).setWeight(10))
+                        .add(LootItem.lootTableItem(MKItems.CAT_FEET.get()).setWeight(10))
+                        .add(LootItem.lootTableItem(MKItems.ARMADILLO_SCALE.get()).setWeight(10))
+                        .add(LootItem.lootTableItem(MKItems.TURTLE_SCALE.get()).setWeight(10))
+                        .add(LootItem.lootTableItem(MKItems.BLAZE_BLOOD.get()).setWeight(10))
+                        .add(LootItem.lootTableItem(MKItems.DOLPHIN_FIN.get()).setWeight(10))
+                        .add(LootItem.lootTableItem(MKItems.RABBIT_FOOT_FOOD.get()).setWeight(10))
+                        .add(LootItem.lootTableItem(MKItems.HORSE_MEAT.get()).setWeight(10))
+                        .add(LootItem.lootTableItem(MKItems.DONKEY_MULE_MEAT.get()).setWeight(10))
+                        .add(LootItem.lootTableItem(MKItems.CRAZY_BLOOD.get()).setWeight(10))
+                        .add(LootItem.lootTableItem(MKItems.STOMACH.get()).setWeight(10))
+                        .add(LootItem.lootTableItem(MKItems.LIVER.get()).setWeight(10))
+                        .add(LootItem.lootTableItem(MKItems.LUNGS.get()).setWeight(10))
+                        .add(LootItem.lootTableItem(MKItems.HEART.get()).setWeight(10))
+                );
+
+        // Add custom loot to the new LootTable
+        LootTable customLootTable = customLoot.build();
+        lootTableBuilder.withPool(LootPool.lootPool()
+                .setRolls(ConstantValue.exactly(1))
+                .add(LootItem.lootTableItem(MKItems.GLOW_SQUID_EYE.get()).setWeight(10))
+                .add(LootItem.lootTableItem(MKItems.BEAR_MEAT.get()).setWeight(10))
+                .add(LootItem.lootTableItem(MKItems.CAT_FEET.get()).setWeight(10))
+                .add(LootItem.lootTableItem(MKItems.ARMADILLO_SCALE.get()).setWeight(10))
+                .add(LootItem.lootTableItem(MKItems.TURTLE_SCALE.get()).setWeight(10))
+                .add(LootItem.lootTableItem(MKItems.BLAZE_BLOOD.get()).setWeight(10))
+                .add(LootItem.lootTableItem(MKItems.DOLPHIN_FIN.get()).setWeight(10))
+                .add(LootItem.lootTableItem(MKItems.RABBIT_FOOT_FOOD.get()).setWeight(10))
+                .add(LootItem.lootTableItem(MKItems.HORSE_MEAT.get()).setWeight(10))
+                .add(LootItem.lootTableItem(MKItems.DONKEY_MULE_MEAT.get()).setWeight(10))
+                .add(LootItem.lootTableItem(MKItems.CRAZY_BLOOD.get()).setWeight(10))
+                .add(LootItem.lootTableItem(MKItems.STOMACH.get()).setWeight(10))
+                .add(LootItem.lootTableItem(MKItems.LIVER.get()).setWeight(10))
+                .add(LootItem.lootTableItem(MKItems.LUNGS.get()).setWeight(10))
+                .add(LootItem.lootTableItem(MKItems.HEART.get()).setWeight(10))
+        );
+
+        // Register the loot table
+        add(entityType, lootTableBuilder);
+    }
+
+
+    // Apply loot table for entities in a tag
+    protected void applyLootTableForTag(TagKey<EntityType<?>> tag, LootTable.Builder lootTable) {
+        ForgeRegistries.ENTITY_TYPES.tags()
+                .getTag(tag)
+                .stream()
+                .forEach(entityType -> add(entityType, lootTable));
+    }
+
+    // Retrieve all registered entities, including custom tags
+    protected Iterable<EntityType<?>> getKnownEntities() {
+        Set<EntityType<?>> knownEntities = new HashSet<>();
+
+        // Explicitly known entities
+        knownEntities.add(EntityType.ARMADILLO);
+        knownEntities.add(EntityType.TURTLE);
+        knownEntities.add(EntityType.BLAZE);
+        knownEntities.add(EntityType.DOLPHIN);
+        knownEntities.add(EntityType.RABBIT);
+        knownEntities.add(EntityType.HORSE);
+        knownEntities.add(EntityType.GLOW_SQUID);
+
+        // Add entities from tags
+        knownEntities.addAll(getEntitiesFromTag(MKTags.MKEntity.FEET));
+        knownEntities.addAll(getEntitiesFromTag(MKTags.MKEntity.LOOTZOMBIE));
+        knownEntities.addAll(getEntitiesFromTag(MKTags.MKEntity.BEARMEAT));
+        knownEntities.addAll(getEntitiesFromTag(MKTags.MKEntity.DMMEAT));
+        knownEntities.addAll(getEntitiesFromTag(MKTags.MKEntity.ORGANMOBS));
+
+        return knownEntities;
+    }
+
+    // Helper function to retrieve entities from a tag
+    protected Set<EntityType<?>> getEntitiesFromTag(TagKey<EntityType<?>> tag) {
+        return ForgeRegistries.ENTITY_TYPES.tags()
+                .getTag(tag)
+                .stream()
+                .map(entityType -> entityType)
+                .collect(Collectors.toSet());
+    }
+
+    // Custom loot tables
+    protected LootTable.Builder createOrganLootTable() {
         return LootTable.lootTable()
                 .withPool(LootPool.lootPool()
                         .setRolls(ConstantValue.exactly(4))
@@ -53,98 +149,80 @@ public class MKEntityLootTableProvider extends EntityLootSubProvider {
                         .add(LootItem.lootTableItem(MKItems.HEART.get()).setWeight(5)));
     }
 
-    private LootTable.Builder createDMMeatLootTable() {
+    protected LootTable.Builder createDMMeatLootTable() {
         return LootTable.lootTable()
                 .withPool(LootPool.lootPool()
                         .setRolls(ConstantValue.exactly(2))
                         .add(LootItem.lootTableItem(MKItems.DONKEY_MULE_MEAT.get())));
     }
 
-    private LootTable.Builder createBearLootTable() {
+    protected LootTable.Builder createBearLootTable() {
         return LootTable.lootTable()
                 .withPool(LootPool.lootPool()
                         .setRolls(ConstantValue.exactly(2))
                         .add(LootItem.lootTableItem(MKItems.BEAR_MEAT.get())));
     }
 
-    private LootTable.Builder createLootZombieLootTable() {
+    protected LootTable.Builder createLootZombieLootTable() {
         return LootTable.lootTable()
                 .withPool(LootPool.lootPool()
                         .setRolls(ConstantValue.exactly(5))
                         .add(LootItem.lootTableItem(MKItems.CRAZY_BLOOD.get())));
     }
 
-    private LootTable.Builder createFeetLootTable() {
+    protected LootTable.Builder createFeetLootTable() {
         return LootTable.lootTable()
                 .withPool(LootPool.lootPool()
                         .setRolls(ConstantValue.exactly(6))
                         .add(LootItem.lootTableItem(MKItems.CAT_FEET.get())));
     }
 
-
-
-
-    private LootTable.Builder createGlowSquidLootTable() {
+    protected LootTable.Builder createGlowSquidLootTable() {
         return LootTable.lootTable()
                 .withPool(LootPool.lootPool()
                         .setRolls(ConstantValue.exactly(2))
                         .add(LootItem.lootTableItem(MKItems.GLOW_SQUID_EYE.get())));
     }
 
-    private LootTable.Builder createHorseLootTable() {
+    protected LootTable.Builder createHorseLootTable() {
         return LootTable.lootTable()
                 .withPool(LootPool.lootPool()
                         .setRolls(ConstantValue.exactly(2))
                         .add(LootItem.lootTableItem(MKItems.HORSE_MEAT.get())));
     }
 
-    private LootTable.Builder createRabbitLootTable() {
+    protected LootTable.Builder createRabbitLootTable() {
         return LootTable.lootTable()
                 .withPool(LootPool.lootPool()
                         .setRolls(ConstantValue.exactly(2))
                         .add(LootItem.lootTableItem(MKItems.RABBIT_FOOT_FOOD.get())));
     }
 
-    private LootTable.Builder createDolphinLootTable() {
+    protected LootTable.Builder createDolphinLootTable() {
         return LootTable.lootTable()
                 .withPool(LootPool.lootPool()
                         .setRolls(ConstantValue.exactly(2))
                         .add(LootItem.lootTableItem(MKItems.DOLPHIN_FIN.get())));
     }
 
-    private LootTable.Builder createBlazeLootTable() {
+    protected LootTable.Builder createBlazeLootTable() {
         return LootTable.lootTable()
                 .withPool(LootPool.lootPool()
                         .setRolls(ConstantValue.exactly(2))
                         .add(LootItem.lootTableItem(MKItems.BLAZE_BLOOD.get())));
     }
 
-    private LootTable.Builder createTurtleLootTable() {
+    protected LootTable.Builder createTurtleLootTable() {
         return LootTable.lootTable()
                 .withPool(LootPool.lootPool()
                         .setRolls(ConstantValue.exactly(2))
                         .add(LootItem.lootTableItem(MKItems.TURTLE_SCALE.get())));
     }
 
-    private LootTable.Builder createArmadilloLootTable() {
+    protected LootTable.Builder createArmadilloLootTable() {
         return LootTable.lootTable()
                 .withPool(LootPool.lootPool()
                         .setRolls(ConstantValue.exactly(2))
                         .add(LootItem.lootTableItem(MKItems.ARMADILLO_SCALE.get())));
     }
-
-    // Custom helper function to apply loot tables to all entities in a tag
-    private void applyLootTableForTag(TagKey<EntityType<?>> tag, LootTable.Builder lootTable) {
-        ForgeRegistries.ENTITY_TYPES.tags()
-                .getTag(tag)  // Get all entities in the tag
-                .stream()
-                .forEach(entityType -> add(entityType, lootTable));  // Apply loot table to each entity in the tag
-    }
-
-    @Override
-    protected Iterable<EntityType<?>> getKnownEntities() {
-        // List all entities you're modifying here
-        return Set.of(EntityType.ZOMBIE, EntityType.CREEPER);
-    }
-
 }
